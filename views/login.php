5f1ca8bbec7d2adf35f33a1b558552ef3662bd03
Josesucho <josesucho.suarez05@gmail.com>
2013-06-09 01:21:33 -0300

    prueba commit

--- views/login.php
+++ views/login.php
@@ -2,7 +2,7 @@
 <html lang="es">
 	<head>
 		<meta charset="utf-8">
-		<title>Iniciar sesión</title>
+		<title>Iniciar sesión Josesucho</title>
 		<link href="css/bootstrap.css" rel="stylesheet">
 		<script src="js/jquery-2.0.0.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
